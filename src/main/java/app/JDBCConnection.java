88aa89c97be7f2b621150e837f8a6cf9b58344df
Nickless-Pandza <s3897262@student.rmit.edu.au>
2021-06-14 12:34:07 +1000

    A

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -15,7 +15,7 @@ import java.sql.Statement;
 public class JDBCConnection {
 
     // Name of database file (contained in database folder)
-    private static final String DATABASE = "jdbc:sqlite:database/Movies.db";
+    private static final String DATABASE = "jdbc:sqlite:database/Covid.db";
 
     public JDBCConnection() {
         System.out.println("Created JDBC Connection Object");
@@ -24,8 +24,8 @@ public class JDBCConnection {
     /**
      * Get all of the Movies in the database
      */
-    public ArrayList<String> getMovies() {
-        ArrayList<String> movies = new ArrayList<String>();
+    public ArrayList<String> getDeaths() {
+        ArrayList<String> deaths = new ArrayList<String>();
 
         // Setup the variable for the JDBC connection
         Connection connection = null;
@@ -39,7 +39,7 @@ public class JDBCConnection {
             statement.setQueryTimeout(30);
 
             // The Query
-            String query = "SELECT * FROM movie";
+            String query = "SELECT * FROM Deaths";
             
             // Get Result
             ResultSet results = statement.executeQuery(query);
@@ -51,13 +51,12 @@ public class JDBCConnection {
                 // We can lookup a column of the a single record in the
                 // result using the column name
                 // BUT, we must be careful of the column type!
-                int id              = results.getInt("mvnumb");
-                String movieName     = results.getString("mvtitle");
-                int year            = results.getInt("yrmde");
-                String type         = results.getString("mvtype");
+                String country      = results.getString("Country_Region");
+                String province     = results.getString("Province_State");
+                int population            = results.getInt("Population");
 
                 // For now we will just store the movieName and ignore the id
-                movies.add(movieName);
+                deaths.add(country);
             }
 
             // Close the statement because we are done with it
@@ -78,176 +77,9 @@ public class JDBCConnection {
         }
 
         // Finally we return all of the movies
-        return movies;
+        return deaths;
     }
 
-        
-    /**
-     * Get all of the Movie Titles in the database
-     */
-    public ArrayList<String> getMovieTitles() {
-        ArrayList<String> titles = new ArrayList<String>();
-
-        // TODO: fill in yourself
-
-        return titles;
-    }
-
-    /**
-     * Count the number of movies in the database
-     */
-    public int countMovies() {
-        int count = 0;
-            // Setup the variable for the JDBC connection
-            Connection connection = null;
-    
-            try {
-                // Connect to JDBC data base
-                connection = DriverManager.getConnection(DATABASE);
-    
-                // Prepare a new SQL Query & Set a timeout
-                Statement statement = connection.createStatement();
-                statement.setQueryTimeout(30);
-    
-                // The Query
-                String query = "SELECT COUNT(*) FROM movie";
-                
-                // Get Result
-                ResultSet results = statement.executeQuery(query);
-            
-                count = results.getInt("COUNT(*)");
-    
-                
-    
-                // Close the statement because we are done with it
-                
-            } catch (SQLException e) {
-                // If there is an error, lets just pring the error
-                System.err.println(e.getMessage());
-            } finally {
-                // Safety code to cleanup
-                try {
-                    if (connection != null) {
-                        connection.close();
-                    }
-                } catch (SQLException e) {
-                    // connection close failed.
-                    System.err.println(e.getMessage());
-                }
-            }
-
-        return count;
-    }
-
-    /**
-     * Get all the movies in the database by a given type.
-     * Note this takes a string of the type as an argument!
-     * This has been implemented for you as an example.
-     * HINT: you can use this to find all of the horror movies!
-     */
-    public ArrayList<String> getMoviesByType(String movieType) {
-        ArrayList<String> movies = new ArrayList<String>();
-
-        // Setup the variable for the JDBC connection
-        Connection connection = null;
-
-        try {
-            // Connect to JDBC data base
-            connection = DriverManager.getConnection(DATABASE);
-
-            // Prepare a new SQL Query & Set a timeout
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-            // The Query
-            String query = "SELECT * FROM movie WHERE mvtype = '" + movieType + "'";
-            System.out.println(query);
-            
-            // Get Result
-            ResultSet results = statement.executeQuery(query);
-
-            // Process all of the results
-            while (results.next()) {
-                String movieName     = results.getString("mvtitle");
-                movies.add(movieName);
-            }
-
-            // Close the statement because we are done with it
-            statement.close();
-        } catch (SQLException e) {
-            // If there is an error, lets just pring the error
-            System.err.println(e.getMessage());
-        } finally {
-            // Safety code to cleanup
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-            }
-        }
-
-        // Finally we return all of the movies
-        return movies;
-    }
-    public ArrayList<String> getMovieTypes()
-    {
-        ArrayList<String> types = new ArrayList<String>();
-
-            // Setup the variable for the JDBC connection
-            Connection connection = null;
-
-            try {
-                // Connect to JDBC data base
-                connection = DriverManager.getConnection(DATABASE);
-
-                // Prepare a new SQL Query & Set a timeout
-                Statement statement = connection.createStatement();
-                statement.setQueryTimeout(30);
-
-                // The Query
-                String query = "SELECT DISTINCT mvtype FROM movie";
-                
-                // Get Result
-                ResultSet results = statement.executeQuery(query);
-
-                // Process all of the results
-                // The "results" variable is similar to an array
-                // We can iterate through all of the database query results
-                while (results.next()) {
-                    // We can lookup a column of the a single record in the
-                    // result using the column name
-                    // BUT, we must be careful of the column type!
-                    String type         = results.getString("mvtype");
-
-                    // For now we will just store the movieName and ignore the id
-                    types.add(type);
-                }
-
-                // Close the statement because we are done with it
-                statement.close();
-            } catch (SQLException e) {
-                // If there is an error, lets just pring the error
-                System.err.println(e.getMessage());
-            } finally {
-                // Safety code to cleanup
-                try {
-                    if (connection != null) {
-                        connection.close();
-                    }
-                } catch (SQLException e) {
-                    // connection close failed.
-                    System.err.println(e.getMessage());
-                }
-            }
-            return types;
-        }
-
-        // Finally we return all of the movies
-        
-    // TODO: Keep adding more methods here to answer all of the questions from the Studio Class activities
         }
 
 
